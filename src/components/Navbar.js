926270af5013e8031843224202cbc01ded0a23c2
possible-generation <ajigbotoshojoshua2000@gmail.com>
2022-05-13 23:21:32 +0100

    add a new link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,9 +10,6 @@ export default function Navbar() {
         <li className='hover:text-green-200 active:text-gray-900'>About</li>
         <li className='hover:text-green-200 active:text-gray-900'>Tours</li>
         <li className='hover:text-green-200 active:text-gray-900'>Tours</li>
-        <li className='hover:text-green-200 active:text-gray-900'>Tours</li>
-        <li className='hover:text-green-200 active:text-gray-900'>Tours</li>
-        <li className='hover:text-green-200 active:text-gray-900'>Tours</li>
       </ul>
     </nav>
   );
